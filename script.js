9bfd6611c1ee356aac79adbb819d801fc657501f
aayush <aayushjuyal12@gmail.com>
2022-04-01 18:49:16 +0530

    Second Commit

--- script.js
+++ script.js
@@ -38,5 +38,5 @@ app.post("/",function(req,res){
   });
 })
 
-app.listen(5500, () => console.log("Server started on port 5500"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port 5500"));
 
